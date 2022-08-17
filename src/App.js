000c8588bc3d2d249ae284827d0d197e15ac11db
junglereef <atila.iglesias@hotmail.com>
2022-08-17 17:02:03 -0300

    homepage complete

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import './App.scss';
 import { Routes, Route } from 'react-router-dom';
-import Layout from './components/Layout';
+import Home from './components/Home'
+// import About from './components/About'
+// import Contact from './components/Contact'
+import Layout from './components/Layout'
+// import Portfolio from './components/Portfolio'
+// import Dashboard from './components/Dashboard'
 
 function App() {
   return (
     <>
     <Routes>
-      <Route path="/" element={<Layout />} />
+      <Route path="/" element={<Layout />} >
+        <Route index element={<Home />} />
+      </Route>
     </Routes>
     </>
   );
