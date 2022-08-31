55935fd78cd29057b3ad3cb270d29e36c73d6141
junglereef <atila.iglesias@hotmail.com>
2022-08-31 09:11:06 -0300

    disable Plugin

--- src/components/Home/Logo/index.js
+++ src/components/Home/Logo/index.js
@@ -1,6 +1,6 @@
 import { useEffect, useRef } from 'react'
-import gsap from 'gsap-trial'
-import DrawSVGPlugin from 'gsap-trial/DrawSVGPlugin'
+// import gsap from 'gsap-trial'
+// import DrawSVGPlugin from 'gsap-trial/DrawSVGPlugin'
 import LogoS from '../../../assets/images/a-logo.png'
 import './index.scss'
 
@@ -10,40 +10,39 @@ const Logo = () => {
   const solidLogoRef = useRef()
 
   useEffect(() => {
-    gsap.registerPlugin(DrawSVGPlugin)
+    // gsap.registerPlugin(DrawSVGPlugin)
 
-    gsap
-      .timeline()
-      .to(bgRef.current, {
-        duration: 1,
-        opacity: 1,
-      })
-      .from(outlineLogoRef.current, {
-        drawSVG: 0,
-        duration: 20,
-      })
+    // gsap
+    //   .timeline()
+    //   .to(bgRef.current, {
+    //     duration: 1,
+    //     opacity: 1,
+    //   })
+    //   .from(outlineLogoRef.current, {
+    //     drawSVG: 0,
+    //     duration: 20,
+    //   })
 
-    gsap.fromTo(
-      solidLogoRef.current,
-      {
-        opacity: 0,
-      },
-      {
-        opacity: 1,
-        delay: 4,
-        duration: 4,
-      }
-    )
+    // gsap.fromTo(
+    //   solidLogoRef.current,
+    //   {
+    //     opacity: 0,
+    //   },
+    //   {
+    //     opacity: 1,
+    //     delay: 4,
+    //     duration: 4,
+    //   }
+    // )
   }, [])
 
   return (
-    <div className="logo-container" ref={bgRef}>
+    <div className="logo-container" /*ref={bgRef}*/>
       <img
-        className="solid-logo"
-        ref={solidLogoRef}
         src={LogoS}
         alt="JavaScript,  Developer"
       />
+       
 
      
     </div>
